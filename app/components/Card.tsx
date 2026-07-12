926a2eb1d07ecb6f762f199de9f97c0b394fa25d
Mohammed Dellihr <mohammeddellihr@maildoo.com>
2026-07-12 10:02:48 +0100

    Update Card.tsx

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -2,7 +2,7 @@
 
 export default function Card({ children, className }: { children: React.ReactNode; className?: string }) {
   return (
-    <div className={`rounded-lg border border-gray-200 bg-gray-50 p-4 dark:border-gray-800 dark:bg-gray-900 ${className || ""}`}>
+    <div className={`rounded border border-gray-200 bg-gray-50 p-4 dark:border-gray-800 dark:bg-gray-900 ${className || ""}`}>
       {children}
     </div>
   )
